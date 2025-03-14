8c12bdd1bd5f2e093c83ab54052ce4f80b8f5d34
Shaulirena <yanivplotnik@gmail.com>
2025-03-14 02:37:36 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,9 @@
 import time
+import sys
 
-print("🚀 הבוט עלה בהצלחה!")
+print("🚀 הבוט עלה בהצלחה!", flush=True)
 
 while True:
-    print("✅ הבוט עדיין חי ומריץ לולאה...")
+    print("✅ הבוט עדיין חי ומריץ לולאה...", flush=True)
+    sys.stdout.flush()  # דוחף החוצה את ההדפסה
     time.sleep(60)
